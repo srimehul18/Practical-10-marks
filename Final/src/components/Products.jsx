2a8a794d05b69615e5680e9c459f382a12d7568e
Mehul Srivastava <srimehul18@gmail.com>
2026-05-04 10:17:55 +0530

    first

---
+++ Final/src/components/Products.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react"
+import React from 'react'
+
+const Products = () => {
+    const [data,setData] = useState("")
+    const API = "https://dummyjson.com/products"
+ 
+      
+        fetch(API)
+        .then(res => res.json())
+        .then(res => setData(res.Products))
+        .catch(console.error())
+        
+        
+    
+    
+    
+
+  return (
+    <div>Products
+        <div>
+        {data.map((item) => (
+            <div key={item.id}>
+                <img src={item.thumbnail}></img>
+            </div>
+        ))
+
+        }
+        </div>
+    </div>
+
+  )
+}
+
+export default Products
